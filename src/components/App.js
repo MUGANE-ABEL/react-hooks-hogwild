61cc9b7534f4ac461807a8d0a164c29b75148fd8
Mugane Abel <muganeabel@gmail.com>
2024-06-20 17:09:33 +0300

    improvements

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import Nav from "./Nav";
+import Filter from "./Filter";
+import HogList from "./HogList";
+
 
 import hogs from "../porkers_data";
 
 function App() {
 	return (
-		<div className="App">
-			<Nav />
+		<body>
+			<div className="App">
+			<div className="ui eight wide column">
+				<Nav />
+			</div>			
+			<div className="pigTile ">
+				<Filter  />				
+			</div>
+			<div className="pigTile">
+				<HogList hogs={hogs} name={hogs.name} image={hogs.image} />
+			</div>					
 		</div>
+		</body>		
 	);
 }
 
