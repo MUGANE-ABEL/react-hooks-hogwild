46e6aba8ba178a934fe1c64223f357b9fce3d3da
Mugane Abel <muganeabel@gmail.com>
2024-06-20 17:12:18 +0300

    Improved

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,42 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Nav from "./Nav";
 import Filter from "./Filter";
 import HogList from "./HogList";
 
-
 import hogs from "../porkers_data";
 
 function App() {
-	return (
-		<body>
-			<div className="App">
-			<div className="ui eight wide column">
-				<Nav />
-			</div>			
-			<div className="pigTile ">
-				<Filter  />				
-			</div>
-			<div className="pigTile">
-				<HogList hogs={hogs} name={hogs.name} image={hogs.image} />
-			</div>					
-		</div>
-		</body>		
-	);
+  const [showGreased, setShowGreased] = useState(false);
+  const [sortBy, setSortBy] = useState("name");
+
+  const hogsToDisplay = hogs
+    .filter((hog) => (showGreased ? hog.greased : true))
+    .sort((hog1, hog2) => {
+      if (sortBy === "weight") {
+        return hog1.weight - hog2.weight;
+      } else {
+        return hog1.name.localeCompare(hog2.name);
+      }
+    });
+
+  return (
+    <div className="ui grid container App">
+      <div className="sixteen wide column centered">
+        <Nav />
+      </div>
+      <div className="sixteen wide column centered">
+        <Filter
+          showGreased={showGreased}
+          onChangeShowGreased={setShowGreased}
+          sortBy={sortBy}
+          onChangeSortBy={setSortBy}
+        />
+      </div>
+      <div className="sixteen wide column centered">
+        <HogList hogs={hogsToDisplay} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
